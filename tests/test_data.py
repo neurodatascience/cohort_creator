541de443cc691a53c8447854a60883e32feede04
Remi Gau <remi_gau@hotmail.com>
2023-12-29 12:00:29 +0100

    add test filter data

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from cohort_creator.data.utils import filter_data
 from cohort_creator.data.utils import is_known_dataset
 from cohort_creator.data.utils import known_datasets_df
 from cohort_creator.data.utils import wrangle_data
@@ -12,3 +13,10 @@ def test_is_known_dataset():
 
 def test_wrangle_data():
     wrangle_data(known_datasets_df())
+
+
+def test_filter_data():
+    df = wrangle_data(known_datasets_df())
+    fitlered_df = filter_data(df)
+
+    assert len(df) == len(fitlered_df)
